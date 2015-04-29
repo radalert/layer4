9272bd77d43284975f6043d57bb2e26ecf938b6c
Lindsay Holmwood <lindsay@holmwood.id.au>
2015-04-29 22:32:39 +1000

    Test dispatch

--- nudger_test.go
+++ nudger_test.go
@@ -21,6 +21,13 @@ func MockApi(bind string) {
 	log.Fatal(http.ListenAndServe(bind, nil))
 }
 
+func MockPacemaker(bind string, requests chan bool) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requests <- true
+	})
+	log.Fatal(http.ListenAndServe(bind, nil))
+}
+
 func TestPollChecks(t *testing.T) {
 	go MockApi("127.0.0.1:42424")
 	config := Config{
@@ -38,4 +45,28 @@ func TestPollChecks(t *testing.T) {
 	}
 
 	t.Logf("checks: %d\n", len(checks))
+}
+
+func TestDispatch(t *testing.T) {
+	requests := make(chan bool)
+	go MockPacemaker("127.0.0.1:42224", requests)
+
+	config := Config{
+		Pacemaker: "http://127.0.0.1:42224",
+	}
+	metrics := make(chan Metric)
+	go Dispatch(config, metrics)
+	go func() {
+		time.Sleep(1 * time.Second)
+		requests <- false
+	}()
+
+	metrics <- Metric{}
+
+	switch <-requests {
+	case true:
+		t.Logf("Dispatched.")
+	case false:
+		t.Fatal("Expected dispatch to pacemaker, got nothing after 1 second.")
+	}
 }
