d6b210ec43464b1a37cddbd86fc6b53bf1fea6e8
Lindsay Holmwood <lindsay@holmwood.id.au>
2015-05-22 18:45:06 +1000

    Initial commit, with a listener for pacemaker alerts

---
+++ taut/taut.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"gopkg.in/alecthomas/kingpin.v1"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Config struct {
+	Bind string
+}
+
+type Alert struct {
+	Org             string   `json:"org"`
+	Check           string   `json:"check"`
+	AnomalyDuration int      `json:"anomaly_duration"`
+	Tags            []string `json:"tags"`
+}
+
+func slackHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+type pacemakerHandler struct {
+	alerts chan Alert
+}
+
+func (ph *pacemakerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("[error] pacemakerHandler: %s\n", err)
+		return
+	}
+	var alert Alert
+	err = json.Unmarshal(body, &alert)
+	if err != nil {
+		log.Printf("[error] pacemakerHandler: Couldn't decode json: %s\n", err)
+		return
+	}
+	ph.alerts <- alert
+}
+
+func sendToSlack(alert chan Alert) {
+	log.Printf("%+v\n", alert)
+}
+
+func Listen(config Config, alerts chan Alert) {
+	router := http.NewServeMux()
+	router.HandleFunc("/slack", slackHandler)
+
+	ph := &pacemakerHandler{alerts: alerts}
+	router.Handle("/pacemaker", ph)
+
+	log.Fatal(http.ListenAndServe(config.Bind, router))
+}
+
+func main() {
+	kingpin.Version("1.0.0")
+	kingpin.Parse()
+
+	fmt.Println("tauters gonna taut taut taut taut")
+
+	alerts := make(chan Alert, 100000)
+	config := Config{Bind: ":8080"}
+	Listen(config, alerts)
+}
---
+++ taut/taut_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+)
+
+func TestMessageFromPacemaker(t *testing.T) {
+	// Setup listen
+	alerts := make(chan Alert, 10)
+	config := Config{Bind: ":8081"}
+	go Listen(config, alerts)
+
+	// Build alert
+	alert := Alert{
+		Org:             "MyCo",
+		Check:           "shizzle.com/health",
+		AnomalyDuration: 180,
+		Tags:            []string{"shizzle", "health"},
+	}
+	body, err := json.Marshal(alert)
+	if err != nil {
+		t.Fatalf("Error encoding alert as JSON: %s\n", err)
+	}
+
+	// Make request
+	url := "http://localhost" + config.Bind + "/pacemaker"
+	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
+	if err != nil {
+		t.Fatalf("Could not create request: %s\n", err)
+	}
+
+	client := &http.Client{Timeout: time.Millisecond * 50}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("HTTP POST should not have failed! Got: %s\n", err)
+	}
+
+	t.Logf("Pacemaker POST response: %+v\n", resp)
+
+	if len(alerts) != 1 {
+		t.Fatalf("Expected %d alert, got %d", 1, len(alerts))
+	}
+}
+
+func TestMessageFromSlack(t *testing.T) {
+	// Setup
+	alerts := make(chan Alert, 10)
+	config := Config{Bind: ":8082"}
+	go Listen(config, alerts)
+
+	// http://requestb.in/1fl5kji1
+
+	// Test
+	values := url.Values{"text": {"text"}, "channel_id": {"C030YR91P"}}
+	url := "http://localhost" + config.Bind + "/slack"
+	resp, err := http.PostForm(url, values)
+	if err != nil {
+		t.Fatalf("HTTP POST should not have failed! Got: %s\n", err)
+	}
+
+	t.Logf("%+v\n", resp)
+}
