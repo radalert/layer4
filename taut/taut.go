783289ef986866ab60f664ecaef0a3d55fe45ecf
Lindsay Holmwood <lindsay@holmwood.id.au>
2015-05-23 21:16:38 +1000

    First pass at sending messages to slack

--- taut/taut.go
+++ taut/taut.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"gopkg.in/alecthomas/kingpin.v1"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 type Config struct {
-	Bind string
+	ListenBind           string
+	Timeout              time.Duration
+	SlackWebhookEndpoint string
+}
+
+type SlackPayload struct {
+	Username string `json:"username"`
+	Text     string `json:"text"`
 }
 
 type Alert struct {
@@ -45,10 +55,6 @@ func (ph *pacemakerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
-func sendToSlack(alert chan Alert) {
-	log.Printf("%+v\n", alert)
-}
-
 func Listen(config Config, alerts chan Alert) {
 	router := http.NewServeMux()
 	router.HandleFunc("/slack", slackHandler)
@@ -56,7 +62,35 @@ func Listen(config Config, alerts chan Alert) {
 	ph := &pacemakerHandler{alerts: alerts}
 	router.Handle("/pacemaker", ph)
 
-	log.Fatal(http.ListenAndServe(config.Bind, router))
+	log.Fatal(http.ListenAndServe(config.ListenBind, router))
+}
+
+func SlackSender(config Config, alerts chan Alert) {
+	for {
+		alert := <-alerts
+		log.Printf("%+v\n", alert)
+		payload := SlackPayload{Username: "Rad Alert", Text: "Anomaly Detected"}
+		value, err := json.Marshal(payload)
+		if err != nil {
+			log.Printf("[error] Slack: JSON marshal: %s\n", err)
+			continue
+		}
+
+		client := &http.Client{Timeout: config.Timeout}
+		urlStr := config.SlackWebhookEndpoint
+		form := url.Values{}
+		form.Add("payload", string(value))
+		req, err := http.NewRequest("POST", urlStr, bytes.NewReader([]byte(form.Encode())))
+		if err != nil {
+			log.Printf("[error] Slack: new request: %s\n", err)
+			continue
+		}
+		_, err = client.Do(req)
+		if err != nil {
+			log.Printf("[error] Slack: client do: %s\n", err)
+			continue
+		}
+	}
 }
 
 func main() {
@@ -66,6 +100,10 @@ func main() {
 	fmt.Println("tauters gonna taut taut taut taut")
 
 	alerts := make(chan Alert, 100000)
-	config := Config{Bind: ":8080"}
+	config := Config{
+		ListenBind:           ":8080",
+		SlackWebhookEndpoint: "https://hooks.slack.com/services/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",
+	}
+	go SlackSender(config, alerts)
 	Listen(config, alerts)
 }
--- taut/taut_test.go
+++ taut/taut_test.go
@@ -4,16 +4,31 @@ import (
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"testing"
 	"time"
 )
 
-func TestMessageFromPacemaker(t *testing.T) {
+func MockSlack(bind string, success chan bool) {
+	http.HandleFunc("/services/ABC/123", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		values, _ := url.ParseQuery(string(body))
+		if len(values.Get("payload")) > 0 {
+			success <- true
+			w.Write([]byte("ok"))
+		} else {
+			w.Write([]byte("not ok"))
+		}
+	})
+	log.Fatal(http.ListenAndServe(bind, nil))
+}
+
+func TestAlertFromPacemaker(t *testing.T) {
 	// Setup listen
 	alerts := make(chan Alert, 10)
-	config := Config{Bind: ":8081"}
+	config := Config{ListenBind: ":8081"}
 	go Listen(config, alerts)
 
 	// Build alert
@@ -29,7 +44,7 @@ func TestMessageFromPacemaker(t *testing.T) {
 	}
 
 	// Make request
-	url := "http://localhost" + config.Bind + "/pacemaker"
+	url := "http://localhost" + config.ListenBind + "/pacemaker"
 	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
 	if err != nil {
 		t.Fatalf("Could not create request: %s\n", err)
@@ -50,21 +65,44 @@ func TestMessageFromPacemaker(t *testing.T) {
 	}
 }
 
+func TestSendToSlack(t *testing.T) {
+	// Setup mock Slack webhook endpoint
+	success := make(chan bool, 10)
+	go MockSlack(":3456", success)
+
+	// Dispatch an alert
+	config := Config{
+		SlackWebhookEndpoint: "http://localhost:3456/services/ABC/123",
+	}
+	alerts := make(chan Alert, 10)
+	go SlackSender(config, alerts)
+	alerts <- Alert{
+		Org:             "MyCo",
+		Check:           "shizzle.com/health",
+		AnomalyDuration: 180,
+		Tags:            []string{"shizzle", "health"},
+	}
+
+	// Test Slack webhook endpoint was called
+	time.Sleep(10 * time.Millisecond)
+	if len(success) != 1 {
+		t.Fatalf("Expected Slack webhook to be called, was not\n")
+	}
+}
+
 func TestMessageFromSlack(t *testing.T) {
 	// Setup
 	alerts := make(chan Alert, 10)
-	config := Config{Bind: ":8082"}
+	config := Config{ListenBind: ":8082"}
 	go Listen(config, alerts)
 
 	// http://requestb.in/1fl5kji1
 
 	// Test
 	values := url.Values{"text": {"text"}, "channel_id": {"C030YR91P"}}
-	url := "http://localhost" + config.Bind + "/slack"
-	resp, err := http.PostForm(url, values)
+	url := "http://localhost" + config.ListenBind + "/slack"
+	_, err := http.PostForm(url, values)
 	if err != nil {
 		t.Fatalf("HTTP POST should not have failed! Got: %s\n", err)
 	}
-
-	t.Logf("%+v\n", resp)
 }
