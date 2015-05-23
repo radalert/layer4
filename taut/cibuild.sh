8b144ce7f464116666237fbdea1e4822a1409c50
Lindsay Holmwood <lindsay@holmwood.id.au>
2015-05-23 23:32:22 +1000

    Add ci bits to build + deploy taut

---
+++ taut/cibuild.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Setup gcloud
+#gcloud components update preview --quiet
+gcloud auth activate-service-account --key-file RadAlert-14dc8a23ab7c.json
+gcloud config set account 42866610386-2ci4tqj3nelqtee3t0s2hrqj68ijpt6v@developer.gserviceaccount.com
+gcloud config set project rad-alert-01
+
+# Build the software
+docker build -t taut .
+
+# Push artifact
+docker tag -f taut gcr.io/rad-alert-01/taut
+gcloud preview docker push gcr.io/rad-alert-01/taut
+
